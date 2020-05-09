85b9f3ec6938da152f75b165a02580641a350cc1
Edouard Paris <m@edouard.paris>
2020-05-09 16:06:02 +0200

    test: good import

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,11 @@
 include!(concat!(env!("OUT_DIR"), "/lnrpc.rs"));
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn lnrpc_imported() {
+        assert_eq!(WalletBalanceRequest {}, WalletBalanceRequest {});
+    }
+}
